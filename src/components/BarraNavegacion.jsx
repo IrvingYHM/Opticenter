14425780c2b589fa1b74354498f5de16a9a81d60
IrvingYHM <hernandezirving725@gmail.com>
2024-01-18 16:33:15 -0600

    Mensaje del commit

--- src/components/BarraNavegacion.jsx
+++ src/components/BarraNavegacion.jsx
@@ -1,144 +1,46 @@
-import { AiOutlineHome } from "react-icons/ai";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineSearch } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import imagen from "../img/logo.jpg";
 import carrito from "../img/carrit.png";
-import burgerMenu from '../img/burgerMenu.png'
-//import closeBtn from '../img/icon-menu-close.svg'
-
+import burgerMenu from "../img/burgerMenu.png";
 
 function App() {
-
- // const [menuClicked, setMenuClicked] = useState(true)
-
-/*   const handleClick = () => {
-    setMenuClicked(prevState => !prevState)
-  } */
-
-
-
-  const handleSearch = () => {
-    // Lógica de búsqueda aquí
-    console.log("Realizar búsqueda...");
-  };
-
   return (
-    <div className="container mt-5" >
+    <div className="container mt-5">
       <nav className="flex flex-col md:flex-row fixed w-full top-0 bg-turquesa py-1" style={{ zIndex: 1000 }}>
-    <div className="flex ">
-        <Link to="/" className="flex items-center ml-8 mb-4 md:mb-0 md:mr-8">
-          <img
-            className="w-18 md:w-25 h-16 md:h-20 flex-wrap rounded-md"
-            src={imagen}
-            alt="logo"
-          />
-        </Link>
-        <img className='w-16 h-16 cursor-pointer sm:hidden ml-auto' src={burgerMenu} alt="Menu hamburguesa" />
+        <div className="flex items-center justify-between w-full md:w-auto md:pl-8 pr-8 md:pr-0">
+          <Link to="/" className="flex items-center">
+            <img className="w-18 md:w-25 h-16 md:h-20 flex-wrap rounded-md" src={imagen} alt="logo" />
+          </Link>
+          <img className="w-16 h-16 cursor-pointer sm:hidden ml-auto" src={burgerMenu} alt="Menu hamburguesa" />
         </div>
 
-
-
-        <div className="md:flex-row items-end justify-between w-full ml-4 md:ml-13">
-          <div className="mb-4 md:mb-0 flex items-center">
-            {/* Barra de búsqueda con icono a la derecha */}
+        <div className="hidden md:flex flex-grow items-center justify-between ml-4 md:ml-13">
+          <div className="flex items-center">
             <div className="hidden md:flex items-center">
-            <input
-              type="text"
-              placeholder="Barra de búsqueda"
-              className="rounded-md px-24 py-1"
-            />
-          <button
-            type="button"
-            onClick={handleSearch}
-            
-          >
-            <AiOutlineSearch className="text-white ml-4 bg-black w-8 h-7 rounded-md" />
-          </button>
-          </div>
+              <input type="text" placeholder="Barra de búsqueda" className="rounded-md px-2 py-1" />
+              <button type="button" onClick={() => console.log("Realizar búsqueda")}>
+                <AiOutlineSearch className="text-white ml-2 bg-black w-6 h-6 rounded-md" />
+              </button>
+            </div>
           </div>
 
-          <div className="flex items-end justify-end gap-12">
-            <div className="hidden md:flex items-end justify-end mb-4 md:mb-0 md:ml-auto gap-2 md:ml-16">
-              <Link
-                to="/inicio"
-                className="hover:bg-blue-900 text-black rounded-md px-4 py-2 font-bold flex items-center"
-              >
-                {" "}
-                <AiOutlineHome size={20} className="mr-2" /> Inicio
-              </Link>
-              <Link
-                to="/lentes"
-                className="hover:bg-blue-900 text-black rounded-md px-4 py-2 font-bold flex items-center"
-              >
+          <div className="flex items-center justify-end gap-8">
+            <Link to="/inicio" className="hover:bg-blue-900 text-black rounded-md px-2 py-2 font-bold flex items-center"> <AiOutlineHome size={20} className="mr-1" /> Inicio </Link>
+            <Link to="/lentes" className="hover:bg-blue-900 text-black rounded-md px-2 py-2 font-bold flex items-center"> Lentes </Link>
+            <Link to="/lentesS" className="hover:bg-blue-900 text-black rounded-md px-2 py-2 font-bold flex items-center"> Lentes solares </Link>
+            <Link to="/accesorios" className="hover:bg-blue-900 text-black rounded-md px-2 py-2 font-bold flex items-center"> Accesorios </Link>
+          </div>
 
-                Lentes
-              </Link>
-              <Link
-                to="/lentesS"
-                className=" hover:bg-blue-900 text-black rounded-md px-2 py-2 font-bold flex items-center"
-              >
-                Lentes solares
-              </Link>
-              <Link
-              to="/accesorios"
-              className="hover:bg-blue-900 text-s rounded-md px-4 py-2 font-bold flex items-center">
-                Accesorios
-              </Link>
-            </div>
-            <div className=" hidden md:flex items-end justify-end gap-2 ml-4 md:ml-32">
-              <a
-                className="bg-aRey hover:bg-blue-900 text-black rounded-md px-4 py-2 font-bold flex"
-                href=""
-              >
-                Gestionar Cita
-              </a>
-              <a
-                className="bg-aRey hover:bg-blue-900 text-black rounded-md px-4 py-2 font-bold flex"
-                href=""
-              >
-                Iniciar Sesion
-              </a>
-              <a
-                className="bg-aRey hover:bg-blue-900 text-black rounded-md px-4 py-2 font-bold"
-                href=""
-              >
-                Registrarse
-              </a>
-              <a href="/carrito">
-                <img
-                  src={carrito}
-                  className="w-10 h-10 ml-2 rounded-md m-1"
-                  alt="carrito"
-                />
-              </a>
-            </div>
+          <div className="hidden md:flex items-center gap-4 ml-4 md:ml-32">
+            <a className="bg-aRey hover:bg-blue-900 text-black rounded-md px-4 py-2 font-bold flex" href=""> Gestionar Cita </a>
+            <a className="bg-aRey hover:bg-blue-900 text-black rounded-md px-4 py-2 font-bold flex" href=""> Iniciar Sesión </a>
+            <a className="bg-aRey hover:bg-blue-900 text-black rounded-md px-4 py-2 font-bold flex" href=""> Registrarse </a>
+            <Link to="/carrito">
+              <img src={carrito} className="w-10 h-10 ml-2 rounded-md m-1" alt="carrito" />
+            </Link>
           </div>
         </div>
-
-        {/* Ícono para dispositivos pequeños */}
-{/*         <button
-          type="button"
-          className="md:hidden inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-          aria-controls="mega-menu"
-          aria-expanded={menuOpen}
-          onClick={toggleMenu}
-        >
-          <span className="sr-only">Abrir menú principal</span>
-          <svg
-            aria-hidden="true"
-            className="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button> */}
-
       </nav>
     </div>
   );
