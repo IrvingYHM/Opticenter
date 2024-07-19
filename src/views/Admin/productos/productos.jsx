00b6323bb248aa877c0848c74360cd72bff0affb
Julio C. <julio.ce01@hotmail.com>
2024-07-19 10:29:33 -0600

    Vistas empleados y barra de navegacion

--- src/views/Admin/productos/productos.jsx
+++ src/views/Admin/productos/productos.jsx
@@ -3,7 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link} from "react-router-dom";
 import Fot from "../../../components/Footer";
-import Barra from "../../../components/Navegacion/barra";
+import Barra from "../../../components/Navegacion/barraAdmin";
 
 
 function App() {
