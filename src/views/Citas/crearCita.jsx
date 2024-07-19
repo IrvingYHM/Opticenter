aa783ca699c14deb0b7cc8e2ff853461223dfc2e
IrvingYHM <hernandezirving725@gmail.com>
2024-07-19 11:52:49 -0600

    modificacion de crear cita

--- src/views/Citas/crearCita.jsx
+++ src/views/Citas/crearCita.jsx
@@ -33,23 +33,6 @@ function parseJwt(token) {
   return JSON.parse(jsonPayload);
 }
 
-// Función para decodificar JWT
-function parseJwt(token) {
-  var base64Url = token.split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    window
-      .atob(base64)
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
 const CrearCita = () => {
   const [mesActual, setMesActual] = useState(new Date());
   const [selectFecha, setSelectFecha] = useState(null);
