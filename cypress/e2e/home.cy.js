47c2c2988b01380367b9b07b396895629447712f
Julio C. <julio.ce01@hotmail.com>
2024-10-29 23:12:28 -0600

    Pruebas en el inicio del sitio web

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,25 @@
 describe('Mi primera prueba', () => {
-  it('Visita la página de ejemplo', () => {
+  //Primera prueba de pagina de inicio
+  it('Visita la página de inicio del sitio web', () => {
     cy.visit('https://opticenter-hu.vercel.app');
   });
+
+  //Prueba de zoom en las imagenes
+  it("Debería mostrar el zoom en la imagen al pasar el mouse", () => {
+    cy.visit("/");
+    cy.get('img[alt="Sunset in the mountains"]').first().trigger("mouseover"); 
+    cy.get('img[alt="Sunset in the mountains"]').first().should("have.class", "zoom");
+    cy.get('img[alt="Sunset in the mountains"]').first().trigger("mouseout"); 
+    cy.get('img[alt="Sunset in the mountains"]').first().should("not.have.class", "zoom"); 
+  });
+
+
+
+  //Prueba de mostrar el footer
+  it("Debería mostrar el footer en la página", () => {
+    cy.visit("/");
+    cy.get("footer").should("exist"); 
+  });
+  
+
 });
