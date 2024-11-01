191d3f6c1fc77d803adea5cb3b447bc2c4f2a215
Julio C. <julio.ce01@hotmail.com>
2024-11-01 06:34:27 -0600

    cambios jest8

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,5 +5,5 @@ module.exports = {
   transform: {
       '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
 };
