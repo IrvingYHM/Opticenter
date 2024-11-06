a7057d1da0114fc87f38e199532214eaa32a241c
Julio C. <julio.ce01@hotmail.com>
2024-11-06 16:02:49 -0600

    cambios 3

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+// babel.config.js o .babelrc
+module.exports = {
+    presets: [
+      '@babel/preset-env',  // Si estás usando ES6+
+      '@babel/preset-react' // Para interpretar JSX
+    ]
+  };
+  
