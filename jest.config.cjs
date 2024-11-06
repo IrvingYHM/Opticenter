796a44ed38eac53c5305f96506c6c575cb4bff0b
Julio C. <julio.ce01@hotmail.com>
2024-11-06 16:22:12 -0600

    cambios 7

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,4 @@
-/* module.exports = {
+module.exports = {
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
@@ -6,5 +6,5 @@
       '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
+  roots: ['<rootDir>/src/test'], // Define el directorio donde están tus pruebas
 };
- */
