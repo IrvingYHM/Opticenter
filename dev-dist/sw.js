e6a40c0c83fcd6e29d5ec73baeb11adf81eab478
Julio C. <julio.ce01@hotmail.com>
2024-11-13 17:23:10 -0600

    actualizacion

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-fde070c5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/offline.html",
-    "revision": "0.77v9ken8kf"
+    "revision": "0.4c31ogilivg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
